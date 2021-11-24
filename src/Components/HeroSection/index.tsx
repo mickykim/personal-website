226753e7f820bb8cc435cfbcaf5fb9123aa1ec8f
Micky Kim <miqueaskimhan@gmail.com>
2021-11-23 20:44:11 -0600

    Removed intro animation

--- src/Components/HeroSection/index.tsx
+++ src/Components/HeroSection/index.tsx
@@ -38,41 +38,41 @@ const HeroSection: React.FC<Props> = ({ id }) => {
     const arrowWrapper = useRef<HTMLDivElement>(null);
     const tl = useRef<GSAPTimeline>();
 
-    useEffect(() => {
-        tl.current = gsap.timeline({ defaults: { ease: "power1.out" } });
+    // useEffect(() => {
+    //     tl.current = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-        // --------- Intro Animations -------------
-        tl.current.to(hideText(".text"), {
-            y: "0%",
-            duration: 1,
-            stagger: 0.8,
-        });
-        tl.current.to(slider.current, {
-            y: "-100%",
-            duration: 1.5,
-            delay: 1,
-        });
-        tl.current.to(window, { scrollTo: 0 }, "-=1.5");
+    //     // --------- Intro Animations -------------
+    //     tl.current.to(hideText(".text"), {
+    //         y: "0%",
+    //         duration: 1,
+    //         stagger: 0.8,
+    //     });
+    //     tl.current.to(slider.current, {
+    //         y: "-100%",
+    //         duration: 1.5,
+    //         delay: 1,
+    //     });
+    //     tl.current.to(window, { scrollTo: 0 }, "-=1.5");
 
-        // ---------- Hero Animations --------------
-        tl.current.to(intro.current, { y: "-100%", duration: 1 }, "-=1");
-        tl.current.fromTo(
-            name(".nickName"),
-            { opacity: 0 },
-            { opacity: 1, duration: 1 }
-        );
-        tl.current.fromTo(
-            name(".fullName"),
-            { opacity: 0 },
-            { opacity: 1, delay: 0.5, duration: 1.5 }
-        );
-        tl.current.fromTo(
-            arrowWrapper.current,
-            { opacity: 0 },
-            { opacity: 1, duration: 1.5 },
-            "-=.5"
-        );
-    }, []);
+    //     // ---------- Hero Animations --------------
+    //     tl.current.to(intro.current, { y: "-100%", duration: 1 }, "-=1");
+    //     tl.current.fromTo(
+    //         name(".nickName"),
+    //         { opacity: 0 },
+    //         { opacity: 1, duration: 1 }
+    //     );
+    //     tl.current.fromTo(
+    //         name(".fullName"),
+    //         { opacity: 0 },
+    //         { opacity: 1, delay: 0.5, duration: 1.5 }
+    //     );
+    //     tl.current.fromTo(
+    //         arrowWrapper.current,
+    //         { opacity: 0 },
+    //         { opacity: 1, duration: 1.5 },
+    //         "-=.5"
+    //     );
+    // }, []);
 
     return (
         <>
